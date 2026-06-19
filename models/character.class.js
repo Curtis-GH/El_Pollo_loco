245f650c1bb24970abe25928fd6973d6e8e92fa8
Curtis <duycurtis1997@gmail.com>
2026-06-19 15:13:44 +0200

    render pepe on console

--- models/character.class.js
+++ models/character.class.js
@@ -1,8 +1,8 @@
 class Character extends MoveableObject{
    
 
-    moveRight(){
-
+    constructor(){
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
 
 
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -2,7 +2,14 @@ class MoveableObject {
     x = 120;
     y = 400;
     img;
+    height = 150;
+    width = 250;
 
+
+    loadImage(path){
+        this.img = new Image (); 
+        this.img.src = path;
+    }
 
      moveRight(){
         console.log('Moving right');
