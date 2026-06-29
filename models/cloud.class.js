24d6153ac0e07821189dff8624ae571827986729
Curtis <duycurtis1997@gmail.com>
2026-06-29 11:53:55 +0200

    fix: remove syntax error W and duplicate moveLeft in cloud.class.js

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -9,17 +9,9 @@ class Cloud extends MoveableObject {
         this.loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 500;
         this.animate();
-
     }
 
     animate() {
-   this.moveLeft();
-}   
-W
-moveLeft(){
- setInterval(() => {
-        this.x -= 0.15;
-    }, 1000 / 60);
-    
-}
+        this.moveLeft();
+    }
 }
