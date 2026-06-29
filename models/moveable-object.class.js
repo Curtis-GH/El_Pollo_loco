24a71fe81c46e6dcafcb80284c24d44c8eda4782
Curtis <duycurtis1997@gmail.com>
2026-06-29 12:05:19 +0200

    feat: store and clear all intervals for clean game stop

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,4 +1,3 @@
-
 class MoveableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
@@ -6,67 +5,74 @@ class MoveableObject extends DrawableObject {
     acceleration = 2;
     energy = 100;
     lastHit = 0;
+    intervals = [];
 
-    
     applyGravity() {
-        setInterval(() => {
+        let id = setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
-            this.y -= this.speedY;
-            this.speedY -= this.acceleration;
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
             }
         }, 1000 / 25);
-}
-
-isAboveGround() {
-    if (this instanceof ThrowableObject) {
-        return true;
-    } else {
-        return this.y < 180;
+        this.intervals.push(id);
     }
-}
-    
 
-isColliding(mo) {
-    return this.x + this.width > mo.x &&
-        this.y + this.height > mo.y &&
-        this.x < mo.x + mo.width &&
-        this.y < mo.y + mo.height;
-}
-
-   hit() {
-    this.energy -= 5;
-    if (this.energy < 0) {
-        this.energy = 0;
-    } else {
-        this.lastHit = new Date().getTime();
+    isAboveGround() {
+        if (this instanceof ThrowableObject) {
+            return true;
+        } else {
+            return this.y < 180;
+        }
     }
-}
 
-isHurt() {
-    let timepassed = new Date().getTime() - this.lastHit; // Difference in ms
-    timepassed = timepassed / 1000; // Difference in s
-    return timepassed < 1;
-}
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x + mo.width &&
+            this.y < mo.y + mo.height;
+    }
 
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
 
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
 
-isDead() {
-    return this.energy == 0;
-}
+    isDead() {
+        return this.energy == 0;
+    }
 
     moveRight() {
-        console.log('Moving right');
-    }
-    moveLeft() {
         setInterval(() => {
+            this.x += this.speed;
+        }, 1000 / 60);
+    }
+
+    moveLeft() {
+        let id = setInterval(() => {
             this.x -= this.speed;
         }, 1000 / 60);
-
+        this.intervals.push(id);
     }
-    playAnimation(images){
-        let i = this.currentImage % images.length; // let i = 7 % 6; => 1, Rest 1
+
+    playAnimation(images) {
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
+    }
+
+    clearIntervals() {
+        this.intervals.forEach(id => clearInterval(id));
+        this.intervals = [];
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -7,6 +7,7 @@ class World {
     camera_x = 0;
     statusBar = new StatusBar();
     throwableObjects = [];
+    intervals = [];
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -16,61 +17,62 @@ class World {
         this.draw();
         this.run();
     }
-   setWorld() {
-    this.character.world = this;
-}
 
-run() {
-    this.level.enemies.forEach(enemy => enemy.animate());
-    setInterval(() => {
-        this.checkCollisions();
-        this.checkThrowObjects();
-    }, 200);
-}
-
-checkThrowObjects() {
-    if (this.keyboard.D) {
-        let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
-        this.throwableObjects.push(bottle);
+    setWorld() {
+        this.character.world = this;
     }
-}
 
+    run() {
+        this.level.enemies.forEach(enemy => enemy.animate());
+        let id = setInterval(() => {
+            this.checkCollisions();
+            this.checkThrowObjects();
+        }, 200);
+        this.intervals.push(id);
+    }
 
+    stopGame() {
+        this.intervals.forEach(id => clearInterval(id));
+        this.intervals = [];
+        this.character.clearIntervals();
+        this.level.enemies.forEach(enemy => enemy.clearIntervals());
+        this.throwableObjects.forEach(bottle => bottle.clearIntervals());
+    }
 
-checkCollisions() {
-    this.level.enemies.forEach((enemy) => {
-        if (this.character.isColliding(enemy)) {
-            this.character.hit();
-            this.statusBar.setPercentage(this.character.energy);
+    checkThrowObjects() {
+        if (this.keyboard.D) {
+            let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+            this.throwableObjects.push(bottle);
         }
-    });
-}
+    }
 
+    checkCollisions() {
+        this.level.enemies.forEach((enemy) => {
+            if (this.character.isColliding(enemy)) {
+                this.character.hit();
+                this.statusBar.setPercentage(this.character.energy);
+            }
+        });
+    }
 
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.ctx.translate(-this.camera_x, 0);
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.throwableObjects);
+        this.ctx.translate(-this.camera_x, 0);
 
-
-   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    this.ctx.translate(this.camera_x, 0);
-    this.addObjectsToMap(this.level.backgroundObjects);
-
-    this.ctx.translate(-this.camera_x, 0); // Back
-    this.addToMap(this.statusBar);
-    this.ctx.translate(this.camera_x, 0); // Forwards
-
-    this.addToMap(this.character);
-    this.addObjectsToMap(this.level.clouds);
-    this.addObjectsToMap(this.level.enemies);
-    this.addObjectsToMap(this.throwableObjects);
-
-    this.ctx.translate(-this.camera_x, 0);
-
-    let self = this;
-    requestAnimationFrame(function () {
-        self.draw();
-    });
-}
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
 
     addObjectsToMap(objects) {
         objects.forEach(o => {
@@ -87,7 +89,6 @@ checkCollisions() {
         }
 
         mo.draw(this.ctx);
-        mo.drawFrame(this.ctx);
 
         if (mo.otherDirection) {
             mo.x = mo.x * -1;
