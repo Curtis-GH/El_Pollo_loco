3fbd934b20240113f2c64f1e33e25f6c6e18729e
Curtis <duycurtis1997@gmail.com>
2026-06-29 11:54:48 +0200

    fix: add drawImage guard to prevent TypeError on unloaded images

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -4,28 +4,29 @@ class DrawableObject {
     img;
     imageCache = {};
     currentImage = 0;
-     height = 150;
+    height = 150;
     width = 100;
-
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
-     draw(ctx){
-       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    draw(ctx) {
+        if (this.img && this.img.complete && this.img.naturalWidth > 0) {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        }
+    }
 
-    }
     drawFrame(ctx) {
-    if (this instanceof Character || this instanceof Chicken) {
-        ctx.beginPath();
-        ctx.lineWidth = '5';
-        ctx.strokeStyle = 'blue';
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.stroke();
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
     }
-}
 
     loadImages(arr) {
         arr.forEach((path) => {
